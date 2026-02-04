d36de77ad24b5eadcded6d7edf926a48fe7ef56f
juannoti <j.notivoliz@alumnos.upm.es>
2026-02-04 19:54:05 +0100

    04-02

---
+++ JS/Campo de Pruebas/main.js
@@ -0,0 +1,16 @@
+console.log('Hola desde el otro lado')
+
+let cocheAzul = 'Coche Azul'
+
+cocheAzul = 'Coche Verde'
+
+console.log (cocheAzul)
+
+const volumen = 16*23*13
+
+console.log(volumen)
+
+const hobbies = ['Cromos', 'Cine', 'Deportes', 'Musicales', 'Coches']
+console.log (hobbies [1])
+hobbies.push('Karaoke')
+console.log (hobbies [5])
